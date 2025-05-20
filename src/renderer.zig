b28b243289287da8ca4c9f09c31ef977f2cd15dc
ErikOakleaf <oakl33f123@gmail.com>
2025-05-20 23:17:27 +0200

    fixed initing renderer

--- src/renderer.zig
+++ src/renderer.zig
@@ -19,13 +19,24 @@ pub const Renderer = struct {
         }
 
         // init window
-        self.window = c.SDL_CreateWindow("Gameboy Emulator", 166 * scale, 144 * scale, c.SDL_WINDOW_OPENGL) orelse return SdlError.SdlInitFailed;
+        self.window = c.SDL_CreateWindow("Gameboy Emulator", 166 * scale, 144 * scale, 0) orelse return SdlError.SdlInitFailed;
 
         // init renderer
         self.renderer = c.SDL_CreateRenderer(self.window, null) orelse return SdlError.SdlInitFailed;
 
-        // texture will be undefined at init
-        self.texture = undefined;
+        // init texture used to render the screen
+        self.texture = c.SDL_CreateTexture(
+            self.renderer,
+            c.SDL_PIXELFORMAT_RGB24,
+            c.SDL_TEXTUREACCESS_STREAMING,
+            166,
+            144,
+        ) orelse return SdlError.SdlInitFailed;
+
+        // make it so that the renderer streches the textures to the output resolution
+        if (!c.SDL_SetRenderLogicalPresentation(self.renderer, 160, 144, c.SDL_LOGICAL_PRESENTATION_STRETCH)) {
+            return SdlError.SdlInitFailed;
+        }
 
         self.scale = scale;
     }
